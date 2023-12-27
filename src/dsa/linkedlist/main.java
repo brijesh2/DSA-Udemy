e13956258d186f85bc61beb129510b66d77d3e3d
brijesh2 <brijesh09singh@gmail.com>
2023-12-27 23:25:57 +0530

    first commit

---
+++ src/dsa/linkedlist/main.java
@@ -0,0 +1,14 @@
+package dsa.linkedlist;
+
+public class main {
+    public static void main(String[] args) {
+        MyLinkedList ml = new MyLinkedList(4);
+        //ml.printAll();
+        ml.appendNode(5);
+        //ml.printAll();
+        ml.prependNode(3);
+        ml.printList();
+        ml.printAll();
+
+    }
+}
